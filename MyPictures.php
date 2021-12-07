c1557378ab85dc4358eeeddbf84a0dce8224f9bc
phil0392 <71399005+phil0392@users.noreply.github.com>
2021-12-06 19:57:47 -0500

    zoomzoom
    weeee

--- MyPictures.php
+++ MyPictures.php
@@ -9,20 +9,37 @@ include("./common/header.php");
 $MyPDO = GetPdo();
 $SID = $_SESSION["UserID"];
 $albums = GetAlbums($SID, $MyPDO);
-if(isset($_POST['Change'])){
+if(isset($_POST['Change']) || isset($_GET['picId'])){
+    if (!isset($dropValue) && isset($_SESSION['dropValue'])){
+       $dropValue=$_SESSION['dropValue']; 
+   }
+        
+    
     if($dropValue != -1){
     $sql = "SELECT `Picture_Id`, `Album_Id`, `FileName`, `Title`, `Description`, `Date_Added` FROM `picture` WHERE Album_Id = :albumId;";
     $pSql = $MyPDO->prepare($sql);
     $pSql->execute(['albumId' => $dropValue]);
     $result = $pSql->fetchAll();
+    $_SESSION['dropValue'] = $dropValue;
     }                    
+    
+}
+if(isset($_GET['picId'])){
+    $picId = $_GET['picId'];
+    $sql = "SELECT `Picture_Id`, `Album_Id`, `FileName`, `Title`, `Description`, `Date_Added` FROM `picture` WHERE Picture_Id = :picId";
+    $pSql = $MyPDO->prepare($sql);
+    $pSql->execute(['picId' => $picId]);
+    
+    $mooshoo = $pSql->fetch(PDO::FETCH_ASSOC);
+    
+}else{
     
 }
 ?>
 
 <form action="MyPictures.php" method="post">
-    <div class="row">
-<select id="dropDown" name="dropValue" class="col-lg-2 text-right">
+    <div class="container" style="padding-left: 300px">
+<select id="dropDown" name="dropValue" class="col-lg-3 text-right" >
                 <option value="-1">Select An Album</option>
                 <?php
                 foreach ($albums as $row) {
@@ -36,18 +53,38 @@ if(isset($_POST['Change'])){
                 ?>
             </select>
     </div>
-    <div class="container">
-    <div class="row" style="">
-        <div class="" style="min-height:400px;min-width:600px; border: 10px solid #969696;background:; display:inline-block; float:left;">
+    <div class="text-center">
+      <?php 
+        if(isset($_GET['picId'])){
+            Echo$mooshoo['Title'];
+        }
+        ?>
+    </div>
+    <div class="container-fluid" style="width:900px;margin:auto; padding-top: 25px;">
+    <div >
+        <div class="" style="min-height:400px;min-width:600px; border: 1px solid #969696;background:; display:inline-block; float:left;">
+        <?php 
+        if(isset($_GET['picId'])){
+            echo"<img src='./Pictures/".$mooshoo['FileName']."'width='600' height='400'>";
+        }
+        ?>
         </div>
-        <div class="" style=" border: 10px solid #969696;min-height:400px; min-width:250px; max-width:100px; overflow-y: auto; background:yellow; float:left;display:inline-block;">
+        <div class="" style=" position:relative;border: 1px solid #969696;min-height:400px; min-width:250px; max-width:100px; overflow-y: auto; background:; float:left;display:inline-block;">
+        <?php 
+        if(isset($_GET['picId'])){
+            Echo$mooshoo['Description'];
+        }
+        ?>
+            <div style="position:absolute;bottom:0px;">
+                <textarea name="comment" rows="4" cols="32" style="resize:none;"></textarea>
+            </div>
         </div>
     </div>
-    <div style="width:850px;  max-height:100px; min-height:100px;border: 10px solid #969696; max-height: 100px; overflow:scroll; overflow-y:hidden;background:;">
+    <div style="max-width:850px; min-width:850px; max-height:135px; min-height:125px;border: 1px solid #969696;  white-space:nowrap;overflow-x:auto;background:;">
      <?php 
-    if(isset($_POST["Change"])){
+    if(isset($_POST["Change"]) || isset($_GET["picId"])){
     foreach ($result as $row){
-                    echo"<img src='./Pictures/".$row['FileName']."'  width='200' height='200'>";
+    echo"<a href='MyPictures.php?picId=".$row['Picture_Id']."'><img src='./thumbnails/".$row['FileName']."' width='150' height='115' style='border:1px solid #969696'></a>";
                         
                 }
     
@@ -56,5 +93,16 @@ if(isset($_POST['Change'])){
     </div>
    
     </div>
-    <input type="submit" name="Change" />
+    <div style="padding-left:300px;">
+    <input type="submit" name="Change" id="clicky" hidden/>
+    </div>
 </form>
+
+<script>
+    document.getElementById("dropDown").addEventListener('change', function (){
+            var clicky = document.getElementById('clicky');
+            clicky.click();
+});
+    
+    </script>
+
