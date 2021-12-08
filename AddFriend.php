7063d82762077bcf89ddbf4f73242a88b8a06a27
phil0392 <71399005+phil0392@users.noreply.github.com>
2021-12-08 18:10:53 -0500

    Update AddFriend.php

--- AddFriend.php
+++ AddFriend.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 
     <?php
     session_start();
@@ -89,7 +84,7 @@ and open the template in the editor.
 
                 </div>
             </form>
-
+        </div>
 
 
 <?php include("./common/footer.php"); ?>
